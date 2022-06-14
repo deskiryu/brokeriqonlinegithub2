b4f7fe86af39970e64eaa085bc16071570b4dc4f
PK <paul.kennedy@pytilia.io>
2022-06-14 15:26:18 +0100

    Issue 98: Fix dialog params

--- BrokerIQ.Online.Server/Pages/Insurance/InsuranceEditBase.cs
+++ BrokerIQ.Online.Server/Pages/Insurance/InsuranceEditBase.cs
@@ -487,7 +487,7 @@ namespace BrokerIQ.Online.Pages
                     var dialogParams = new DialogParameters();
 
                     var customer = await CustomerService.GetCustomer(customerId);
-                    dialogParams.Add("Client", customer.Name);
+                    dialogParams.Add("Customer", customer.Name);
 
                     var fileNames = new List<string>(); 
                     var memoryStreams = new List<MemoryStream>();
