d1c78317d67e3f3ea6b4631e012062d200c5b1d0
desmond mcgivern <dmc_givern@hotmail.com>
2022-06-21 16:31:02 +0100

    oops

--- BrokerIQ.Online.Server/Services/NotificationService.cs
+++ BrokerIQ.Online.Server/Services/NotificationService.cs
@@ -148,9 +148,9 @@ namespace BrokerIQ.Online.Server.Services
 
         public async Task<int> GetNewBrokerNotificationsCountByBrokerId()
         {
-            var user = await this.accountservice.getuser();
-            this.requestproviderservice.token = user?.token;
-            var brokerid = user.masterbrokerid;
+            var user = await this.accountService.GetUser();
+            this.requestProviderService.Token = user?.Token;
+            var brokerId = user.MasterBrokerId;
 
             var answer = await this.requestProviderService.Get<int>(this.notificationUrl + $"/brokerById/newcount?brokerId={brokerId}");
             return 0;
