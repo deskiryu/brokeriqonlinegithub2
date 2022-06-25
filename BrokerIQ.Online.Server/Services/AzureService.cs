efaa33f743e88314005de60243e3df74f5e9faa3
desmond mcgivern <dmc_givern@hotmail.com>
2022-06-25 11:55:38 +0100

    No need for this

--- BrokerIQ.Online.Server/Services/AzureService.cs
+++ BrokerIQ.Online.Server/Services/AzureService.cs
@@ -85,7 +85,6 @@ namespace BrokerIQ.Online.Server.Services
             var result = await TransferStreamToAzureBlob(blobClient, fileName, stream, brokerId);
             var tags = new Dictionary<string,string>();
             tags.Add("Broker", brokerId.ToString());
-            tags.Add("SendDate", ((DateTimeOffset)sendDate).ToString("g"));
          
             await blobClient.SetTagsAsync(tags);
             return result;
