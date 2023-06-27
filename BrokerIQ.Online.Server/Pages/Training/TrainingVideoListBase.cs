cd0b83dae3ba9cad06c903766f5becadd1b3fe81
Marco Santos <marco@brokeriq.co.uk>
2023-06-27 12:43:30 +0100

    Add new page to list training videos

---
+++ BrokerIQ.Online.Server/Pages/Training/TrainingVideoListBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace BrokerIQ.Online.Pages
+{
+    using System.Threading.Tasks;
+    using BrokerIQ.Online.Server.Models;
+    using BrokerIQ.Online.Services.Interface;
+    using Microsoft.AspNetCore.Components;
+
+    public class TrainingVideoListBase : ComponentBase
+    {
+        [Inject]
+        protected ITrainingVideoService TrainingVideoService { get; set; }
+
+        protected List<TrainingVideo> Videos { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            Videos = await TrainingVideoService.GetVideos();
+        }
+    }
+}
