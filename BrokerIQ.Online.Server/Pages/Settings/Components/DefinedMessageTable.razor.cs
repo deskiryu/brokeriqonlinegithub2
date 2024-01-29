8f999c5a899bdd38fc3a1a34c10c84fd6b417f4c
Marco Santos <marco@brokeriq.co.uk>
2024-01-29 13:39:23 +0000

    Add drag drop ordering for broker template

--- BrokerIQ.Online.Server/Pages/Settings/Components/DefinedMessageTable.razor.cs
+++ BrokerIQ.Online.Server/Pages/Settings/Components/DefinedMessageTable.razor.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
 using BrokerIQ.Dto.Enum;
 using BrokerIQ.Dto.Models;
 using BrokerIQ.Online.Models;
 using BrokerIQ.Online.Server.Components;
 using BrokerIQ.Online.Services.Interface;
-using Microsoft.AspNetCore.Components;
+
 using MudBlazor;
+using System;
 
 namespace BrokerIQ.Online.Server.Pages.Settings.Components
 {
@@ -28,9 +31,14 @@ namespace BrokerIQ.Online.Server.Pages.Settings.Components
         [Parameter]
         public Online.Models.Broker Broker { get; set; }
 
-        private ICollection<BrokerDefinedMessageDto> DefinedMessages { get; set; }
+        private IEnumerable<BrokerDefinedMessageDto> DefinedMessages { get; set; }
 
         protected override async Task OnInitializedAsync()
+        {
+            await RefreshMessages();
+        }
+
+        private async Task RefreshMessages()
         {
             DefinedMessages = new List<BrokerDefinedMessageDto>(await BrokerDefinedMessageService.GetAllForCurrentBroker());
         }
@@ -130,5 +138,65 @@ namespace BrokerIQ.Online.Server.Pages.Settings.Components
 
             await ReloadDefinedMessages();
         }
+
+        #region Row drag and drop
+        private BrokerDefinedMessageDto? draggedItem;
+
+        private int? enterIndex;
+        private bool? enterAfterDropZone;
+
+        private void DragStart(BrokerDefinedMessageDto model)
+        {
+            draggedItem = model;
+        }
+
+        private void DragEnter(int index, bool isAfterDropZone)
+        {
+            if (draggedItem?.SortOrder == index)
+            {
+                enterIndex = null;
+                enterAfterDropZone = null;
+            }
+            else
+            {
+                enterIndex = index;
+                enterAfterDropZone = isAfterDropZone;
+            }
+        }
+
+        private async Task DropAsync(int index)
+        {
+            await DragEnd();
+        }
+
+        private async Task DragEnd()
+        {
+            if (enterIndex.HasValue && draggedItem.SortOrder != enterIndex.Value)
+            {
+                draggedItem.SortOrder = enterIndex.Value;
+
+                await BrokerDefinedMessageService.Update(draggedItem);
+
+                await RefreshMessages();
+
+                StateHasChanged();
+            }
+
+            draggedItem = null;
+            enterIndex = null;
+            enterAfterDropZone = null;
+        }
+
+        private bool IsEntering(int index)
+        {
+            return enterIndex == index;
+        }
+
+        private string IsDraggable()
+        {
+            return "true";
+        }
+        #endregion
+
     }
 }
