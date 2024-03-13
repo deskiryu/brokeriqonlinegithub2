f4d4b4031231234a0a18040aca3a14b1008e1f5f
Marco Santos <marco@brokeriq.co.uk>
2024-03-13 10:02:36 +0000

    Create UI for document vault type management

---
+++ BrokerIQ.Online.Server/Pages/Settings/Components/DocumentVaultTypeTable.razor.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+using BrokerIQ.Dto.Enum;
+using BrokerIQ.Dto.Models;
+using BrokerIQ.Online.Models;
+using BrokerIQ.Online.Server.Components;
+using BrokerIQ.Online.Services.Interface;
+
+using MudBlazor;
+using BrokerIQ.Dto.UpdateDto;
+
+namespace BrokerIQ.Online.Server.Pages.Settings.Components
+{
+    public partial class DocumentVaultTypeTable : ComponentBase
+    {
+        [Inject]
+        private IDialogService DialogService { get; set; }
+
+        [Inject]
+        private ISnackbar Snackbar { get; set; }
+
+        [Inject]
+        public IDocumentVaultTypeService DocumentVaultTypeService { get; set; }
+
+        [Parameter]
+        public User User { get; set; }
+
+        [Parameter]
+        public Online.Models.Broker Broker { get; set; }
+
+        private ICollection<DocumentVaultTypeDto> DocumentVaultTypes { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            DocumentVaultTypes = new List<DocumentVaultTypeDto>(await DocumentVaultTypeService.GetAllForCurrentBroker());
+        }
+
+        private async Task RemoveDocumentVaultType(DocumentVaultTypeDto vaultType)
+        {
+            var parameters = new DialogParameters
+            {
+                { "ContentText", "Do you really want to delete these Document Vault type? This process cannot be undone." },
+                { "ButtonText", "Delete" },
+                { "Color", Color.Error }
+            };
+
+            var dialogOptions = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+            var result = await DialogService.Show<ConfirmationDialog>("Delete", parameters, dialogOptions).Result;
+
+            if (!result.Cancelled)
+            {
+                await DocumentVaultTypeService.Delete(vaultType);
+            }
+
+            await ReloadDocumentVaultTypes();
+        }
+
+        private async Task ReloadDocumentVaultTypes()
+        {
+            DocumentVaultTypes = new List<DocumentVaultTypeDto>(await DocumentVaultTypeService.GetAllForCurrentBroker());
+
+            StateHasChanged();
+        }
+
+        private async Task EditDocumentVaultType(DocumentVaultTypeDto vaultType)
+        {
+            var operation = vaultType.Id == 0 ? "Create" : "Edit";
+            var title = $"{operation} {vaultType.Name} vault type";
+            var parameters = new DialogParameters
+            {
+                { "VaultType", vaultType }
+            };
+
+            var options = new DialogOptions() { MaxWidth = MaxWidth.Medium, FullWidth = true };
+
+            var result = await DialogService.Show<DocumentVaultTypeDialog>(title, parameters, options).Result;
+
+            if (!result.Cancelled)
+            {
+                DocumentVaultTypeDto updated = result.Data as DocumentVaultTypeDto;
+
+                var wasSuccessfull = false;
+                if (vaultType.Id == 0)
+                {
+                    wasSuccessfull = await DocumentVaultTypeService.Create(new CreateDocumentVaultTypeDto()
+                    {
+                        BrokerId = updated.BrokerId,
+                        Name = updated.Name,
+                        AdditionalDetail = updated.AdditionalDetail
+                    });
+                }
+                else
+                {
+                    wasSuccessfull = await DocumentVaultTypeService.Update(new UpdateDocumentVaultTypeDto()
+                    {
+                        Id = updated.Id,
+                        BrokerId = updated.BrokerId,
+                        Name = updated.Name,
+                        AdditionalDetail = updated.AdditionalDetail
+                    });
+                }
+
+                // TODO : Re introduce these when a fix for the parsing error has been found
+                // if (wasSuccessfull)
+                // {
+                //     Snackbar.Add("Reminder option was removed.", Severity.Success);
+                // }
+                // else
+                // {
+                //     Snackbar.Add("Reminder options update failed. Please try again.", Severity.Error);
+                // } 
+            }
+
+            await ReloadDocumentVaultTypes();
+        }
+    }
+}
---
+++ BrokerIQ.Online.Server/Services/DocumentVaultTypeService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using BrokerIQ.Dto.Models;
+using BrokerIQ.Dto.UpdateDto;
+using BrokerIQ.Online.Services.Abstract;
+using BrokerIQ.Online.Services.Interface;
+
+namespace BrokerIQ.Online.Services
+{
+    public class DocumentVaultTypeService : IDocumentVaultTypeService
+    {
+        private const string API_CONTROLLER = "DocumentVaultType";
+
+        private readonly IMapper mapper;
+        private readonly IRequestProviderService requestProviderService;
+        private readonly IAccountService accountService;
+
+        public DocumentVaultTypeService(IMapper mapper, IRequestProviderService requestProviderService, IAccountService accountService)
+        {
+            this.mapper = mapper;
+            this.requestProviderService = requestProviderService;
+            this.accountService = accountService;
+        }
+
+        public async Task<IEnumerable<DocumentVaultTypeDto>> GetAllForCurrentBroker()
+        {
+            var brokerId = await GetCurrentBrokerId();
+            try
+            {
+                return await requestProviderService.Get<IEnumerable<DocumentVaultTypeDto>>($"{API_CONTROLLER}?brokerid={brokerId}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Get: exception {ex.Message}");
+            }
+
+            return Array.Empty<DocumentVaultTypeDto>();
+        }
+
+        private async Task<int> GetCurrentBrokerId()
+        {
+            var user = await accountService.GetUser();
+            requestProviderService.Token = user?.Token;
+
+            return user.MasterBrokerId;
+        }
+
+        public async Task<bool> Create(CreateDocumentVaultTypeDto vaultType)
+        {
+            vaultType.BrokerId = await GetCurrentBrokerId();
+
+            bool response = false;
+            try
+            {
+                response = await requestProviderService.Post<CreateDocumentVaultTypeDto, bool>(API_CONTROLLER, vaultType);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CreateOrCreate: exception {ex.Message}");
+            }
+            return response;
+        }
+
+        public async Task<bool> Update(UpdateDocumentVaultTypeDto vaultType)
+        {
+            vaultType.BrokerId = await GetCurrentBrokerId();
+
+            bool response = false;
+            try
+            {
+                response = await requestProviderService.Put<UpdateDocumentVaultTypeDto, bool>(API_CONTROLLER, vaultType);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"UpdateOrCreate: exception {ex.Message}");
+            }
+            return response;
+        }
+
+        public async Task<bool> Delete(DocumentVaultTypeDto vaultType)
+        {
+            try
+            {
+                return await requestProviderService.Delete(API_CONTROLLER, vaultType.Id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Delete: exception {ex.Message}");
+            }
+
+            return false;
+        }
+    }
+}
