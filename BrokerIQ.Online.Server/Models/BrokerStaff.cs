28f30c074258cc2629963de1f37cadee7e89b8a8
Marco Santos <marco@brokeriq.co.uk>
2024-08-29 17:24:12 +0100

    Remove required property from phone number

--- BrokerIQ.Online.Server/Models/BrokerStaff.cs
+++ BrokerIQ.Online.Server/Models/BrokerStaff.cs
@@ -23,8 +23,7 @@ namespace BrokerIQ.Online.Models
         public string LastName { get; set; }
 
         [MaxLength(15, ErrorMessage = "The phone number must be 15 letters or less")]
-        [Required(ErrorMessage = "Mobile no. is required in format +4407xxxxxxxxx")]
-        [RegularExpression("^((\\+447)) ?\\d{9}$", ErrorMessage = "Please enter valid phone no in format +447xxxxxxxx")]
+        [RegularExpression("^((\\+447)) ?\\d{9}$", ErrorMessage = "Please enter valid phone no in format +447xxxxxxxxx")]
         public string TwoFactorPhoneNumber { get; set; }
 
         public TwoFactorEnum TwoFactorType { get; set; }
