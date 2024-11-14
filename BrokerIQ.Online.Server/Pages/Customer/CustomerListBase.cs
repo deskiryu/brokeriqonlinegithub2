c898df0f2832a537af24bf5fe49d250ace4845c1
desmond mcgivern <dmc_givern@hotmail.com>
2024-11-14 12:26:37 +0000

    fromsettings

--- BrokerIQ.Online.Server/Pages/Customer/CustomerListBase.cs
+++ BrokerIQ.Online.Server/Pages/Customer/CustomerListBase.cs
@@ -69,10 +69,6 @@ namespace BrokerIQ.Online.Pages
         public int CustomerCategory { get; set; }
         public int AgeRange { get; set; }
 
-        public bool FixedHeader { get; set; }
-        public bool FixedFooter { get; set; }
-        public string ScrollHeight { get => "500px"; }
-
         protected int ProfilingOption { get; set; } = int.MaxValue;
 
         protected Dictionary<int, string> EmployeeColour { get; set; } = new Dictionary<int, string>();
@@ -98,8 +94,6 @@ namespace BrokerIQ.Online.Pages
 
         protected override async Task OnInitializedAsync()
         {
-            FixedHeader = true;
-            FixedFooter=true;
             try
             {
                 SelectFilled = false;
