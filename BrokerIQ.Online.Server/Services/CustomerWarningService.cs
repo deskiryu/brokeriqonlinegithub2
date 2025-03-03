49be216dcb52ce0e8d1466355b6c1f0ab559499a
Marco Santos <marco@brokeriq.co.uk>
2025-03-03 13:55:34 +0000

    Update customer to show all warnings

---
+++ BrokerIQ.Online.Server/Services/CustomerWarningService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using BrokerIQ.Dto.Dto;
+using BrokerIQ.Dto.Enum;
+using BrokerIQ.Online.Server.Services.Base;
+using BrokerIQ.Online.Services.Abstract;
+using BrokerIQ.Online.Services.Interface;
+
+namespace BrokerIQ.Online.Services
+{
+    public class CustomerWarningService : BIQService, ICustomerWarningService
+    {
+        private readonly string CustomerWarningUrl = "CustomerWarning";
+        private readonly IMapper mapper;
+
+        public CustomerWarningService(IRequestProviderService requestProviderService, IMapper mapper, IAccountService accountService)
+            : base(accountService, requestProviderService)
+
+        {
+            this.mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CustomerWarningDto>> GetForCustomer(int id)
+        {
+            var answer = await this._requestProviderService.Get<IEnumerable<CustomerWarningDto>>($"{this.CustomerWarningUrl}/customer/{id}");
+            return this.mapper.Map<IEnumerable<CustomerWarningDto>>(answer);
+        }
+    }
+}
