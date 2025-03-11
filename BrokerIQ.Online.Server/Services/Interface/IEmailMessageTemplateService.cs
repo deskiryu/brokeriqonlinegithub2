e0d582640a0310ddc5a1cf53164c40fc02dc80e6
Marco Santos <marco@brokeriq.co.uk>
2025-03-11 10:59:42 +0000

    Create ui for customisable email message

---
+++ BrokerIQ.Online.Server/Services/Interface/IEmailMessageTemplateService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BrokerIQ.Dto.Dto;
+using BrokerIQ.Dto.UpdateDto;
+
+namespace BrokerIQ.Online.Services.Interface
+{
+    public interface IEmailMessageTemplateService
+    {
+        Task<IEnumerable<EmailMessageTemplateDto>> GetAllForBroker(int brokerId);
+
+        Task<bool> Update(UpdateEmailMessageTemplateDto EmailMessageTemplate);
+    }
+}
