1c0096d5fbcbf59e39370c1df23a791dcb920005
Marco Santos <marco@brokeriq.co.uk>
2025-05-09 15:58:45 +0100

    Split logic for load and new messages

--- BrokerIQ.Online.Server/Pages/Customer/CustomerDetailBase.cs
+++ BrokerIQ.Online.Server/Pages/Customer/CustomerDetailBase.cs
@@ -388,17 +388,27 @@ namespace BrokerIQ.Online.Pages
                 return;
             }
 
-            if (!IsInChatTab())
+            if (IsInChatTab())
+            {
+                if (loadMessages)
+                {
+                    if (LastChatPageLoaded == 0)
+                    {
+                        Chat = await LoadChatMessages();
+                    }
+                    else
+                    {
+                        var newMessages = await LoadNewMessages();
+
+                        Chat.Messages = newMessages.Concat(Chat.Messages).OrderByDescending(m => m.Id).ToList();
+                    }
+                }
+            }
+            else
             {
                 UnReadChat = response.Data;
                 ChatBadgeColour = UnReadChat > 0 ? MudBlazor.Color.Error : MudBlazor.Color.Transparent;
                 ChatBadgeDot = UnReadChat == 0;
-            }
-
-            if (loadMessages || IsInChatTab())
-            {
-                LastChatPageLoaded = 0;
-                Chat = await LoadChatMessages();
             }
 
             await InvokeAsync(StateHasChanged);
@@ -511,7 +521,8 @@ namespace BrokerIQ.Online.Pages
                     else
                     {
                         AlertService.Error("Notification sending failed");
-                    };
+                    }
+                    ;
                 }
             }
             else
@@ -1394,6 +1405,12 @@ namespace BrokerIQ.Online.Pages
             AllChatMessagesLoaded = !chat.MoreMessagesAvailable;
 
             return chat;
+        }
+
+        private async Task<IList<ChatMessage>> LoadNewMessages()
+        {
+            var MaxId = Chat.Messages.Max(m => m.Id);
+            return (await ChatService.GetPaged(Customer.Id, Broker.Id, 1, ChatPageSize, IsInChatTab())).Messages.Where(m => m.Id > MaxId).ToList();
         }
 
         protected async Task<IEnumerable<BrokerDefinedMessage>> OnTemplateFilter(string value)
