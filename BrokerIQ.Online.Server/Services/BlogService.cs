d12e4bd95d7520d52d910dc59c98257f9e8b66a1
desmondmcgivern <dmc_givern@hotmail.com>
2025-05-14 12:56:31 +0100

    got some blogs

---
+++ BrokerIQ.Online.Server/Services/BlogService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using BrokerIQ.Dto.Models;
+using BrokerIQ.Dto.UpdateDto;
+using BrokerIQ.Online.Server.Services.Base;
+using BrokerIQ.Online.Services.Abstract;
+using BrokerIQ.Online.Services.Interface;
+
+namespace BrokerIQ.Online.Services
+{
+    public class BlogService : BIQService, IBlogService
+    {
+        private const string API_CONTROLLER = "Blog";
+
+        public BlogService(IAccountService accountService, IRequestProviderService requestProviderService)
+            : base(accountService, requestProviderService)
+        {
+        }
+
+        public async Task<IEnumerable<BlogDto>> GetAll()
+        {
+            try
+            {
+                return await _requestProviderService.Get<IEnumerable<BlogDto>>($"{API_CONTROLLER}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Get: exception {ex.Message}");
+            }
+
+            return Array.Empty<BlogDto>();
+        }
+    }
+}
---
+++ BrokerIQ.Online.Server/Services/Interface/IBlogService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using BrokerIQ.Dto.Models;
+using BrokerIQ.Dto.UpdateDto;
+
+namespace BrokerIQ.Online.Services.Interface
+{
+    public interface IBlogService
+    {
+        Task<IEnumerable<BlogDto>> GetAll();
+
+    }
+}
