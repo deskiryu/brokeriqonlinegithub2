7bc76c1ef0fa4a0d7fc2f3348bc4954b64fee6b1
desmondmcgivern <dmc_givern@hotmail.com>
2025-07-02 15:34:39 +0100

    keep old dashboard for existing users

---
+++ BrokerIQ.Online.Server/Services/Interface/IOldChartDataService.cs
@@ -0,0 +1,26 @@
+﻿using BrokerIQ.Dto.Model.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrokerIQ.Online.Services.Interface
+{
+    public interface IOldChartDataService
+    {
+        Task<AppConversionDto> GetInvitesSentAndConverted(int brokerId);
+
+        Task<List<(DateTime, int, int, int, int, int)>> GetInvitesSentAndConvertedSequence(int brokerId);
+
+        List<(DateTime, int, int)> GetNotificationsSent();
+
+        public List<(string, int)> GetBrokersPerYear();
+
+        public List<(string, int)> GetAppUsersPerYear();
+
+        public List<(string, int)> GetPremiumsManagedPerYear();
+
+        Task<List<(string, int)>> GetTotalLogins(int brokerId);
+    }
+}
