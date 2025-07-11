a89a4c53f8ea4d033a8f4f4d0ec97c9494b74a6e
Marco Santos <marco@brokeriq.co.uk>
2025-07-11 12:51:32 +0100

    Map pipedrive id to customer

--- BrokerIQ.Online.Server/Mapper/ReviewItMapper.cs
+++ BrokerIQ.Online.Server/Mapper/ReviewItMapper.cs
@@ -92,7 +92,8 @@ namespace BrokerIQ.Online.Mapper
                 .ForMember(d => d.ConnectedToCustomerId, action => action.MapFrom(s => s.ConnectedToCustomerId))
                 .ForMember(d => d.AppointmentRemindersAllowed, action => action.MapFrom(s => !s.AppointmentReminderOptOut))
                 .ForMember(d => d.WealthTypeId, action => action.MapFrom(s => s.WealthTypeId))
-                .ForMember(d => d.HasConnection, action => action.MapFrom(s => s.HasConnection));
+                .ForMember(d => d.HasConnection, action => action.MapFrom(s => s.HasConnection))
+                .ForMember(d => d.PipedriveId, action => action.MapFrom(s => s.PipedriveId));
 
             CreateMap<Customer, UpdateCustomerDto>()
                 .ForMember(d => d.Id, action => action.MapFrom(s => s.Id))
