41bd8c6d560c1a083bb549e8fdac9ca2f3982250
Marco Santos <marco@brokeriq.co.uk>
2025-09-02 10:39:12 +0100

    Use Pipedrive call to check connection

--- BrokerIQ.Online.Server/Services/PipeDriveService.cs
+++ BrokerIQ.Online.Server/Services/PipeDriveService.cs
@@ -26,6 +26,11 @@ public class PipedriveService : IPipedriveService
         return await this.requestProviderService.Post($"{Url}/auth", code);
     }
 
+    public async Task<bool> IsConnected()
+    {
+        return await this.requestProviderService.Get<bool>($"{Url}/isconnected");
+    }
+
     public async Task<bool> Disconnect()
     {
         return await this.requestProviderService.Delete($"{Url}");
@@ -42,4 +47,6 @@ public class PipedriveService : IPipedriveService
     {
         await this.requestProviderService.Post<int, CustomerDto>($"{this.Url}/chatsync", id);
     }
+
+
 }
